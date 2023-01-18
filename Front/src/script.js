fd5fe5c75bf2a80968494d12889b9be16645f0c5
abdallah <abddoullah.bouzhar@gmail.com>
2023-01-18 17:22:54 +0100

    carousel

--- Front/src/script.js
+++ Front/src/script.js
@@ -1,4 +1,4 @@
-window.addEventListener("scroll", ()=>{
+/* window.addEventListener("scroll", ()=>{
     const nav = document.getElementById('nav')
     const navheight = nav.getBoundingClientRect().height
     if(window.pageYOffset>navheight){
@@ -6,4 +6,27 @@ window.addEventListener("scroll", ()=>{
     }else{
         nav.classList.remove('sticky')
     }
+})
+ */
+const carousel = document.getElementById('carousel')
+const carouselWidth = carousel.getBoundingClientRect().width
+
+const cards = document.querySelectorAll(".test")
+    cards.forEach((card)=>{
+        card.style.width = ((carouselWidth-16)/4)+'px'
+        
+    })
+console.log(window.innerWidth)
+window.addEventListener("resize", ()=>{
+    const carouselWidth = carousel.getBoundingClientRect().width
+    const cards = document.querySelectorAll(".test")
+    cards.forEach((card)=>{
+        const colCount = Math.floor(carouselWidth/ 270)
+        /* if (colCount>4) {
+            colCount = 4
+        } */
+        card.style.width = ((carouselWidth-(colCount*4))/colCount)+'px'
+        //console.log(card.getBoundingClientRect().width);
+    })
+    
 })
