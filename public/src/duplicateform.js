ac6dd75edb36e439a372d9d6fcbbb9128ac567e7
abdallah <abddoullah.bouzhar@gmail.com>
2023-02-03 21:32:27 +0100

    form validating photos

--- public/src/duplicateform.js
+++ public/src/duplicateform.js
@@ -18,6 +18,10 @@ class formsHandlerClass {
     const div = document.createElement("div");
     div.innerHTML = product_form.innerHTML;
     const data_inputs = div.querySelectorAll(".data_inputs");
+    const photos_feedback = div.querySelector("#photos_feedback-"+(this.count-1));
+    photos_feedback.id= 'photos_feedback-'+this.count
+    const feedback_message = div.querySelector("#feedback_message-"+(this.count-1));
+    feedback_message.id= 'feedback_message-'+this.count
     data_inputs.forEach((input) => {
       let name = input.getAttribute("name");
       name = name.substring(0, name.length - 3);
@@ -60,15 +64,37 @@ class formsHandlerClass {
     if (count === "") {
       count = this.count;
     }
-    console.log(count);
+
     const nom = document.getElementById("nom-" + count);
     const prix_achat = document.getElementById("prix_achat-" + count);
     const prix_vente = document.getElementById("prix_vente-" + count);
     const quantite = document.getElementById("quantite-" + count);
     const categorie = document.getElementById("categorie-" + count);
     const description = document.getElementById("description-" + count);
+    const photos = document.getElementById("photos[]-" + count);
+    let files = photos.files;
+    files = Array.from(files);
 
     var errors = [];
+
+    if (files.length == 0) {
+      errors.push("nom is required");
+      this.inputStyleHandlerError(photos);
+      const photos_feedback = document.getElementById(
+        "photos_feedback-" + count
+      );
+      const feedback_message = document.getElementById(
+        "feedback_message-" + count
+      );
+      feedback_message.innerText = "veuillez choisir une photo";
+      photos_feedback.classList.remove("hidden");
+    } else {
+      this.inputStyleHandlerAccepted(photos);
+      const photos_feedback = document.getElementById(
+        "photos_feedback-" + count
+      );
+      photos_feedback.classList.add("hidden");
+    }
 
     if (nom.value === "" || nom.value === "NULL") {
       errors.push("nom is required");
@@ -77,26 +103,38 @@ class formsHandlerClass {
       this.inputStyleHandlerAccepted(nom);
     }
 
-/*     if (prix_achat.value === "" || prix_achat.value === "NULL") {
+    /*     if (prix_achat.value === "" || prix_achat.value === "NULL") {
       errors.push("nom is required");
       this.inputStyleHandlerError(prix_achat);
     } else {
       this.inputStyleHandlerAccepted(prix_achat);
     } */
     console.log(this.isNumber(prix_achat.value));
-    if (!this.isNumber(prix_achat.value) || prix_achat.value === "" || prix_achat.value === "NULL") {
+    if (
+      !this.isNumber(prix_achat.value) ||
+      prix_achat.value === "" ||
+      prix_achat.value === "NULL"
+    ) {
       errors.push("nom is required");
       this.inputStyleHandlerError(prix_achat);
     } else {
       this.inputStyleHandlerAccepted(prix_achat);
     }
-    if (!this.isNumber(prix_vente.value) || prix_vente.value === "" || prix_vente.value === "NULL") {
+    if (
+      !this.isNumber(prix_vente.value) ||
+      prix_vente.value === "" ||
+      prix_vente.value === "NULL"
+    ) {
       errors.push("nom is required");
       this.inputStyleHandlerError(prix_vente);
     } else {
       this.inputStyleHandlerAccepted(prix_vente);
     }
-    if (!this.isNumber(quantite.value) || quantite.value === "" || quantite.value === "NULL") {
+    if (
+      !this.isNumber(quantite.value) ||
+      quantite.value === "" ||
+      quantite.value === "NULL"
+    ) {
       errors.push("nom is required");
       this.inputStyleHandlerError(quantite);
     } else {
@@ -122,8 +160,6 @@ class formsHandlerClass {
   }
 }
 
-
-
 let formsHandler = new formsHandlerClass();
 
 form_duplicator.addEventListener("click", () => {
@@ -135,9 +171,36 @@ form_duplicator.addEventListener("click", () => {
 submit_form.addEventListener("click", (e) => {
   let count = formsHandler.count;
   let validationResults = formsHandler.validateAll(count);
-  
+
   if (!validationResults) {
     e.preventDefault();
-}
+  }
 
+  
 });
+
+const input = document.getElementById("photos-0");
+const upladed_images_div = document.getElementById("upladed_images_div-0");
+
+/* 
+input.addEventListener("change", ()=> {
+
+    let files = input.files
+    files = Array.from(files)
+  
+    files.forEach(file => {
+        
+        const img_element = document.createElement('img')
+        img_element.classList.add('border','border-black','max-w-[50px]')
+        upladed_images_div.appendChild(img_element)
+    
+      const reader = new FileReader();
+      
+      reader.onload = function() {
+        img_element.src = reader.result
+      };
+      
+      reader.readAsDataURL(file); 
+    });
+
+}); */
