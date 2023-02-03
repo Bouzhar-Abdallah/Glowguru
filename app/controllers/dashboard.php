96229be1f33351924eb72341fcfa0e47a7585bfb
abdallah <abddoullah.bouzhar@gmail.com>
2023-02-04 00:33:27 +0100

    products data fetched by ajax succes

--- app/controllers/dashboard.php
+++ app/controllers/dashboard.php
@@ -13,10 +13,29 @@ class Dashboard extends Controller
     {
         $data = [];
         
+        $this->view('dashboard','forajax',$data);
+    }
+    public function old($a = '', $b = '', $c = '')
+    {
+        $data = [];
+        
         $Produits_dashboard = new Produits_dashboard;
         $data = $Produits_dashboard->selectAll();
         
         $this->view('dashboard','products_table',$data);
+    }
+    public function getProductsAjax($a = '', $b = '', $c = '')
+    {
+        $data = [];
+        
+        $Produits_dashboard = new Produits_dashboard;
+        $data = $Produits_dashboard->selectAll();
+        foreach ($data as $key => $value) {
+            $data[$key]['photo'] = base64_encode($value['photo']);
+        }
+        
+        header('Content-Type: application/json');
+        echo json_encode($data);
     }
     
     public function categories()
